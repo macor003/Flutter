20e07f1597722143ca0ffb87fa0b5203c77eeb26
Mario Ortega <mario.ortega@sofka.com.co>
2020-03-13 09:25:48 -0500

    Se finaliza practica de contador con tres botones

--- Section-5/contador/lib/src/pages/contador_page.dart
+++ Section-5/contador/lib/src/pages/contador_page.dart
@@ -8,13 +8,13 @@ class ContadorPage extends StatefulWidget {
 class _ContadorPageState extends State {
   final _estiloTexto = new TextStyle(fontSize: 25);
 
-  int _conteo = 10;
+  int _conteo = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Bienvenido'),
+        title: Text('Bienvenido $_conteo'),
         centerTitle: true,
       ),
       body: Center(
@@ -31,17 +31,58 @@ class _ContadorPageState extends State {
           ),
         ],
       )),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          _conteo++;
-          print('$_conteo');
-          setState(() {
-            
-          });
-        },
-      ),
+      floatingActionButton: _crearBotones(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
+  }
+
+  Widget _crearBotones() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        SizedBox(
+          width: 5.0,
+        ),
+        FloatingActionButton(
+          child: Icon(Icons.exposure_zero),
+          onPressed: _resetear,
+        ),
+        Expanded(
+            child: SizedBox(
+          width: 5.0,
+        )),
+        FloatingActionButton(
+          child: Icon(Icons.remove),
+          onPressed: _restar,
+        ),
+        SizedBox(
+          width: 5.0,
+        ),
+        FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: _agregar,
+        ),
+        SizedBox(
+          width: 5.0,
+        )
+      ],
+    );
+  }
+
+  void _agregar() {
+    setState(() => _conteo++);
+  }
+
+  void _restar() {
+    if (_conteo > 0) {
+      _conteo--;
+    } else {
+      _conteo = _conteo;
+    }
+    setState(() {});
+  }
+
+  void _resetear() {
+    setState(() => _conteo = 0);
   }
 }

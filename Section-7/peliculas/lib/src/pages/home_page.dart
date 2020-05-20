dd51088fa918947eff772412d0461ed97e8f427b
Mario Ortega <mario.ortega@sofka.com.co>
2020-05-19 23:41:27 -0500

    Se agrega modelo de pelicula, se crea petición http y se muestra en dispositivo

---
+++ Section-7/peliculas/lib/src/pages/home_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:peliculas/src/providers/peliculas_provider.dart';
+import 'package:peliculas/src/widgets/card_swiper_widget.dart';
+
+class HomePage extends StatelessWidget {
+  final peliculasProvider = new PeliculasProvider();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Películas en Cine'),
+          backgroundColor: Colors.blue[900],
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.search), onPressed: () {})
+          ],
+        ),
+        body: Container(
+          child: Column(
+            children: <Widget>[_swiperTarjetas()],
+          ),
+        ));
+  }
+
+  Widget _swiperTarjetas() {
+    return FutureBuilder(
+      future: peliculasProvider.getEnCines(),
+      builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
+        if (snapshot.hasData) {
+          return CardSwiper(peliculas: snapshot.data);
+        } else {
+          return Container(height: 400.0,child: Center(child: CircularProgressIndicator()));
+        }
+      },
+    );
+  }
+}

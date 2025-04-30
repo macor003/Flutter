94f60e88e01c2e26768a43325698cff3ca260e98
Mario Ortega <mcor63@gmail.com>
2025-04-29 23:08:51 -0500

    Se agrega mas funcionalidad a app de practica counter

--- hello_wold_app/lib/presentation/screens/counter/counter_screen.dart
+++ hello_wold_app/lib/presentation/screens/counter/counter_screen.dart
@@ -8,27 +8,60 @@ class CounterScreen extends StatefulWidget {
 }
 
 class _CounterScreenState extends State<CounterScreen> {
+  int counter = 0;
+  void increment() {
+    setState(() {
+      counter++;
+    });
+  }
+  void decrement() {
+    setState(() {
+      counter--;
+    });
+  }
+  
+  void reset() {
+    setState(() {
+      counter = 0;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Counter'),
         centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: reset,
+            icon: const Icon(Icons.refresh),
+          )
+        ],
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('10', style: TextStyle(fontSize: 160, fontWeight: FontWeight.w100)), 
-            Text('Clicks', style: TextStyle(fontSize: 25))
+            Text('$counter', style: TextStyle(fontSize: 160, fontWeight: FontWeight.w100)), 
+            Text(counter == 1 ? 'Click' : 'Clicks', style: TextStyle(fontSize: 25))
           ]
         )
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          
-        },
-        child: const Icon(Icons.add),
+      floatingActionButton: Column(
+        children: [
+          const Spacer(),
+          FloatingActionButton(
+            backgroundColor: Colors.red.shade100,
+            onPressed: decrement,
+            child: const Icon(Icons.remove),
+          ),
+          const SizedBox(height: 10,),
+          FloatingActionButton(
+            onPressed: increment,
+            child: const Icon(Icons.add),
+          ),
+        ],
       ),
     );
   }
